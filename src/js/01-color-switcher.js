d4bec565658beba4e0a0e4d7ecd487114da79981
AleksandrTikka <alexandertikka@gmail.com>
2022-06-13 23:39:44 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+}
 
+let timerId = null;
+
+refs.btnStart.addEventListener('click', onBtnStart);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    },1000)
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+}
+
+function onBtnStop() {
+    clearInterval(timerId);
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
