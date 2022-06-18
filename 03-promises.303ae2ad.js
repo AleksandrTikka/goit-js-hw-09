e579b46e77a5216c3685410437b697f393ad71d7
AleksandrTikka <alexandertikka@gmail.com>
2022-06-18 22:15:32 +0000

    Deploying to gh-pages from @ AleksandrTikka/goit-js-hw-09@bf478e5db254dcfb977eb7e8f61ff114121a21fb 🚀

--- 03-promises.303ae2ad.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){e.prevent.default(),console.log(e.target.delay.value),e.target.delay.value;e.target.step.value;for(var t=e.target.amount.value,a=1;a<=t;a+=1);}));
-//# sourceMappingURL=03-promises.303ae2ad.js.map
