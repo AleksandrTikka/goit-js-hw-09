12aaa84b2344f577fe2feb03bfaa4b357bd70390
AleksandrTikka <alexandertikka@gmail.com>
2022-07-02 00:14:18 +0300

    hw9 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -21,17 +21,43 @@ const options = {
   onClose(selectedDates) {
       console.log(selectedDates[0]);
       
-    if (selectedDates[0] <= this.defaultDate) {
-     Notify.failure("Please choose a date in the future"); 
+    if (selectedDates[0] <= new Date()) {
+      Notify.failure("Please choose a date in the future"); 
+      refs.btn.disabled = true;
       } else {
       selectedTime = selectedDates[0];
+      refs.btn.disabled = false;
       
       }
   },
 };
 console.log(options.defaultDate);
 
-// refs.input.addEventListener('input', flatpickr);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+
+
 refs.btn.addEventListener('click', onBtnCalculate);
 
 let timerId = null;
@@ -61,30 +87,10 @@ function addValue(deltaTime) {
     refs.seconds.textContent = addLeadingZero(convertMs(deltaTime).seconds);
 };
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
 flatpickr("#datetime-picker", options);
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 };
+
+
